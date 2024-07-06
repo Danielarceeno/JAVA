6ce97955810bb795f83c49e2f2e8763965f48402
Danielarceeno <daniel7ar@gmail.com>
2024-07-06 16:56:24 -0300

    main

--- POO/src/Banco.java
+++ POO/src/Banco.java
@@ -4,6 +4,21 @@ public class Banco{
         p1.setNumConta(1111);
         p1.setDono("Daniel");
         p1.abrirConta("CC");
+       
+
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Bruna");
+        p2.abrirConta("CP");
+       
+
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(100);
+
+        p1.fecharConta();
+
         p1.statusAtual();
+        p2.statusAtual();
     }
 }

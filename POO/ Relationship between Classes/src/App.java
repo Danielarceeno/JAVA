9a4eb6272459502418f68963f980d2bb96747775
Danielarceeno <daniel7ar@gmail.com>
2024-07-08 20:33:18 -0300

    main

---
+++ POO/ Relationship between Classes/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lutador l[] = new Lutador[6];
+        l[0] = new Lutador("PRETTY BOY", "FRANÇA", 31, 1.75f, 68.9f, 11, 2, 1);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f,57.8f,14,2,3);
+        l[2] = new Lutador ("SnapShadow", "EUA", 35, 1.65f, 80.9f, 12,2,1);
+
+        l[2].status();
+    }
+}
---
+++ POO/ Relationship between Classes/src/Lutador.java
@@ -0,0 +1,141 @@
+public class Lutador {
+
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias, derrotas, empates;
+
+    // Construtor
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.peso = pe;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    // Apresentar o lutador
+    public void apresentar() {
+        System.out.println("Lutador: " + getNome());
+        System.out.println("Origem: " + getNacionalidade());
+        System.out.println(getIdade() + " anos");
+        System.out.println("Pesando " + getPeso() + " kg");
+        System.out.println("Ganhou: " + getVitorias());
+        System.out.println("Perdeu: " + getDerrotas());
+        System.out.println("Empatou: " + getEmpates());
+    }
+
+    // Status do lutador
+    public void status() {
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+        System.out.println("Ganhou " + this.getVitorias() + " vezes");
+        System.out.println("Perdeu " + this.getDerrotas() + " vezes");
+        System.out.println("Empatou " + this.getEmpates());
+    }
+
+    // Getters e Setters
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(getEmpates() + 1);
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura(){
+        return altura;
+    }
+
+    public void setAltura(int altura){
+        this.altura = altura;
+    }
+
+    public void ganharLuta() {
+        setVitorias(getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        setDerrotas(getDerrotas() + 1);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    private void setCategoria() {
+        if (peso < 52.2) {
+            this.categoria = "Inválido";
+        } else if (peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (peso <= 83.9) {
+            this.categoria = "Médio";
+        } else if (peso <= 120) {
+            this.categoria = "Pesado";
+        } else {
+            this.categoria = "Inválido";
+        }
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
